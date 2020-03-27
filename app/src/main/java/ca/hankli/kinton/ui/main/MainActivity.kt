79494448c53d28c3c259a2d78d42817c116c4a16
Hank Li <hankli0130@gamil.com>
2020-03-27 15:19:00 -0400

    Add a bottom navigation

--- app/src/main/java/ca/hankli/kinton/ui/main/MainActivity.kt
+++ app/src/main/java/ca/hankli/kinton/ui/main/MainActivity.kt
@@ -2,12 +2,28 @@ package ca.hankli.kinton.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ca.hankli.kinton.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Navigation Host Fragment
+        val host = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        setBottomNavMenu(navController)
+    }
+
+    private fun setBottomNavMenu(navController: NavController) {
+        bottom_nav_view.setupWithNavController(navController)
     }
 }
