a3b79687c2a7c9de2d91277faca40a899f588674
Hank Li <hankli0130@gamil.com>
2020-03-27 15:42:14 -0400

    Change LinkFragment to CardFragment Add icons to bottom navigation

---
+++ app/src/main/java/ca/hankli/kinton/ui/main/card/CardFragment.kt
@@ -0,0 +1,10 @@
+package ca.hankli.kinton.ui.main.card
+
+import ca.hankli.kinton.R
+import ca.hankli.kinton.ui.base.BaseFragment
+
+class CardFragment : BaseFragment() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_card
+}
