f1bd615f408dc568f14685a68ed5dd22850c5dce
Hank Li <hankli0130@gamil.com>
2020-05-07 22:44:51 -0400

    Renew Reward Fragment's view

--- app/src/main/java/ca/hankli/kinton/ui/main/reward/RewardFragment.kt
+++ app/src/main/java/ca/hankli/kinton/ui/main/reward/RewardFragment.kt
@@ -3,11 +3,11 @@ package ca.hankli.kinton.ui.main.reward
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import ca.hankli.kinton.R
 import ca.hankli.kinton.ui.util.BaseFragment
-import ca.hankli.kinton.ui.util.MarginItemDecoration
 import ca.hankli.kinton.util.extension.visit
-import kotlinx.android.synthetic.main.fragment_reward.view.*
+import kotlinx.android.synthetic.main.fragment_reward.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RewardFragment : BaseFragment() {
@@ -23,21 +23,10 @@ class RewardFragment : BaseFragment() {
 
     private val viewModel: RewardViewModel by viewModel()
 
-    private val adapter: RewardAdapter = RewardAdapter()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.reward_list.apply {
-            setHasFixedSize(true)
-            adapter = this@RewardFragment.adapter
-            addItemDecoration(
-                MarginItemDecoration(resources.getDimension(R.dimen.padding_size_12).toInt())
-            )
+        view_scan.setOnClickListener {
+            Toast.makeText(requireContext(), "scan QR code", Toast.LENGTH_SHORT).show()
         }
-
-//        adapter.apply {
-//            items = viewModel.getRewardItems()
-//            notifyDataSetChanged()
-//        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
