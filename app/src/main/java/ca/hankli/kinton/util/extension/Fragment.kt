40f3cbb46d64097e55b44cd821b99b00e64bd33e
Hank Li <hankli0130@gamil.com>
2020-05-12 15:52:36 -0400

    Remove Activity extension Update Fragment extension

--- app/src/main/java/ca/hankli/kinton/util/extension/Fragment.kt
+++ app/src/main/java/ca/hankli/kinton/util/extension/Fragment.kt
@@ -1,24 +1,30 @@
 package ca.hankli.kinton.util.extension
 
+import android.content.Intent
+import android.net.Uri
 import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
+import android.os.Vibrator
 import androidx.annotation.IntRange
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import ca.hankli.kinton.util.isPermissionNotGranted
 
-fun Fragment.visit(uri: String) = activity?.visit(uri)
+fun Fragment.visit(uri: String) {
+    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
+}
 
-fun Fragment.vibrate(
-    milliseconds: Long = 100L,
-    amplitude: Int = VibrationEffect.DEFAULT_AMPLITUDE
-) = activity?.vibrate(milliseconds, amplitude)
-
+fun Fragment.vibrate(millis: Long = 100L, amplitude: Int = DEFAULT_AMPLITUDE) {
+    getSystemService(requireContext(), Vibrator::class.java)?.run {
+        if (hasVibrator()) vibrate(VibrationEffect.createOneShot(millis, amplitude))
+    }
+}
 
 fun Fragment.askForPermissions(
     permissions: Array<out String>,
     @IntRange requestCode: Int,
     doOnAllGranted: () -> Unit
 ) {
-
     val deniedPermissions = mutableListOf<String>()
 
     for (permission in permissions) {
